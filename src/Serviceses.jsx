31cbabb4913a0acdad672ef23db2071ae7c444fc
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-10-07 17:45:10 +0500

    added compare slider

--- src/Serviceses.jsx
+++ src/Serviceses.jsx
@@ -8,6 +8,10 @@ import vacumcleaner from "/vacumcleaner.svg";
 import vishilka from "/vishilka.svg";
 import skuter from "/skuter.svg";
 import { useTranslation } from "react-i18next";
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
 
 const Service = ({ img, name }) => {
   return (
@@ -40,35 +44,52 @@ const Proccess = ({ icon, title, desc }) => {
 };
 
 const Serviceses = () => {
-  const {t, i18n} = useTranslation();
+  const { t, i18n } = useTranslation();
   return (
     <div className="mt-32 items-center px-10" id="serviceses">
       <h1 data-aos="fade-right" className="mb-7">
         {t("xizmat")}
       </h1>
-      <div data-aos="fade-up" className="grid grid-cols-1 md:grid md:grid-cols-3">
+      <div
+        data-aos="fade-up"
+        className="grid grid-cols-1 md:grid md:grid-cols-3"
+      >
         <Service img={"plisos.png"} name={t("faqatGilam")} />
         <Service img={"dazmol.png"} name={t("barchaMebel")} />
         <Service img={"parda.png"} name={t("barchaParda")} />
         <Service img={"gilam.png"} name={t("barchaGilam")} />
         <Service img={"sochiq.png"} name={t("barchaAdyol")} />
       </div>
-      <h1 data-aos="fade-down-right" className="mb-7 text-center text-[45px] font-bold text-[#000929b3] text-wrap">
+      <h1
+        data-aos="fade-down-right"
+        className="mb-7 text-center text-[45px] font-bold text-[#000929b3] text-wrap"
+      >
         {t("erishayotganNatija")}
       </h1>
-      <div data-aos="fade-up-right" className="flex justify-center">
-        <img
+      <div data-aos="fade-up-right" className="md:flex md:justify-center">
+        {/* <img
           src="/natija.jpg"
           alt="natija"
-          className="w-1/3 object-cover inline"
+          className="w-full md:w-1/3 md:object-cover md:inline"
         />
         <img
           src="/natija2.jpg"
           alt="natija"
-          className="w-1/3 object-cover inline"
+          className="hidden md:w-1/3 md:object-cover md:inline"
+        /> */}
+        <ReactCompareSlider
+          itemOne={
+            <ReactCompareSliderImage src="/natija.jpg" alt="Image one" className="w-full md:w-1/3 md:object-cover md:inline" />
+          }
+          itemTwo={
+            <ReactCompareSliderImage src="/natija2.jpg" alt="Image two" className="hidden md:w-1/3 md:object-cover md:inline" />
+          }
         />
       </div>
-      <h1 data-aos="flip-right" className="mt-32 mb-4 text-center text-[45px] font-bold text-[#000929b3]">
+      <h1
+        data-aos="flip-right"
+        className="mt-32 mb-4 text-center text-[45px] font-bold text-[#000929b3]"
+      >
         {t("ishLavha")}
       </h1>
       <div className="flex flex-col md:flex md:flex-row gap-5 w-full overflow-hidden overflow-x-scroll">
@@ -91,23 +112,21 @@ const Serviceses = () => {
           <source src="/video6.mp4" />
         </video>
       </div>
-      <h1 data-aos="fade-right" className="mb-10 mt-32 text-[45px] font-bold text-[#193365] font-ibm">
+      <h1
+        data-aos="fade-right"
+        className="mb-10 mt-32 text-[45px] font-bold text-[#193365] font-ibm"
+      >
         {t("qandayIshlaydi")}
       </h1>
-      <div data-aos="fade-up" className="grid grid-cols-1 gap-10 md:grid md:grid-cols-4">
-        <Proccess
-          icon={call}
-          title={t("call")}
-          desc={
-            t("callBody")
-          }
-        />
+      <div
+        data-aos="fade-up"
+        className="grid grid-cols-1 gap-10 md:grid md:grid-cols-4"
+      >
+        <Proccess icon={call} title={t("call")} desc={t("callBody")} />
         <Proccess
           icon={delivery}
           title={t("buyurtma")}
-          desc={
-            t("buyurtmaBody")
-          }
+          desc={t("buyurtmaBody")}
         />
         <Proccess
           icon={deal}
@@ -135,25 +154,34 @@ const Serviceses = () => {
           desc={t("yetkazibBerishBody")}
         />
         <div className="mt-4">
-          <a href="#contact" className="bg-[#04a2ba] text-white py-4 px-10 text-3xl rounded-xl hover:text-white">
+          <a
+            href="#contact"
+            className="bg-[#04a2ba] text-white py-4 px-10 text-3xl rounded-xl hover:text-white"
+          >
             {t("buyurtmaBering")}
           </a>
-          <p className="text-[#9e9e9e] text-xl pt-3 mt-3">
-            {t("obrodir")}
-          </p>
+          <p className="text-[#9e9e9e] text-xl pt-3 mt-3">{t("obrodir")}</p>
         </div>
       </div>
-      <h1 data-aos="fade-right" className="mb-10 mt-32 text-[45px] font-bold text-black font-ibm">
+      <h1
+        data-aos="fade-right"
+        className="mb-10 mt-32 text-[45px] font-bold text-black font-ibm"
+      >
         {t("ishJarayoni")}
       </h1>
-      <div data-aos="zoom-in" className="grid grid-cols-1 md:grid md:grid-cols-4 gap-2">
+      <div
+        data-aos="zoom-in"
+        className="grid grid-cols-1 md:grid md:grid-cols-4 gap-2"
+      >
         <div className="md:overflow-hidden md:row-span-2 md:col-span-2 md:w-[720px] md:h-[710px] rounded-xl">
           <img
             src="/galareya1.jpg"
             alt="galareya"
             className="w-[450px] rounded-xl md:w-full md:transition md:duration-150 md:ease-linear md:hover:scale-110"
           />
-          <div className="hidden group-hover:flex items-center justify-center z-20"><Button /></div>
+          <div className="hidden group-hover:flex items-center justify-center z-20">
+            <Button />
+          </div>
         </div>
         <div className="overflow-hidden">
           <img
